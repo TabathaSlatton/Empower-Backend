462d33e679967d9de818e8fda4632789db0d8362
Tabatha Slatton <valbatha@gmail.com>
2020-12-22 11:11:10 -0700

    updated users controller for jwt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,21 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
+      @user = User.create(user_params)
+      if @user.valid?
+        # passing encode_token a payload of user id
+        @token = encode_token(user_id: @user.id)
+        # using built-in rails status codes
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
       end
+    end
     
-      private
-      def user_params
+    private
+
+    def user_params
         params.require(:user).permit(:username, :password, :email, :profile_img_url, :first_name, :last_name, :point_wallet)
-      end
+    end
 end
