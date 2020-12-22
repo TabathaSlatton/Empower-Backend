c2b9d29e0fcca18e6a205b9467749857f0a1155b
Tabatha Slatton <valbatha@gmail.com>
2020-12-21 23:38:30 -0700

    created user model

---
+++ db/migrate/20201222063150_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :profile_img_url
+      t.integer :point_wallet
+
+      t.timestamps
+    end
+  end
+end
