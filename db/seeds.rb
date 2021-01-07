4634d9b39f961a5eb68f514fb1d69de92092526e
Tabatha Slatton <valbatha@gmail.com>
2021-01-06 18:51:39 -0700

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(point_cost: 1200, img_url: "https://cdn.cliqueinc.com/posts/264792/new-outfit-ideas-264792-1533935250623-image.700x0c.jpg", name: "New Outfit")
+Product.create(point_cost: 15, img_url: "https://images.dailyhive.com/20200709113548/netflix-canada1.jpg", name: "30 Minutes of Netflix")
+Product.create(point_cost: 200, img_url: "https://i1.wp.com/www.hadviser.com/wp-content/uploads/2020/02/1-short-angled-bob-B6DM3BvJ20r.jpg?resize=983%2C1099&ssl=1", name: "Haircut")
+Product.create(point_cost: 250, img_url: "https://sa1s3optim.patientpop.com/assets/images/provider/photos/2125892.jpg", name: "Get a massage")
+Product.create(point_cost: 50, img_url: "https://www.bathroomsandmorestore.co.uk/wp-content/uploads/2017/10/candle1.jpg", name: "Bubble Bath")
+Product.create(point_cost: 3000, img_url: "https://img.huffingtonpost.com/asset/5e14bcfd250000d6ddd320d1.jpeg?cache=Wp6UYpWMlz&ops=1778_1000", name: "Dance Lessons")
+Product.create(point_cost: 20000, img_url: "https://www.fodors.com/wp-content/uploads/2019/07/woman-relaxing-on-deck-large-photo.jpg", name: "Vacation")
+Product.create(point_cost: 80, img_url: "https://static.onecms.io/wp-content/uploads/sites/24/2017/03/full-sun-2427301_peoni_043cg-2000.jpg", name: "Flowers")
+Product.create(point_cost: 80, img_url: "https://www.cancer.net/sites/cancer.net/files/styles/blog_media/public/donate-help-blog.png?itok=tSbeI2pI", name: "Donate $10")
+Product.create(point_cost: 75, img_url: "https://assets.cakematernity.com/wp-content/uploads/mom-me-time.jpg", name: "1 hour of me time")
